57bd0f56d2438345c4d6816a3fdd51203bb08a7b
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-08-16 15:50:06 +0530

    Fixed previous logic

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,19 +1,25 @@
 // Birthday Function
 let birthday = setTimeout(function () {
   var current = new Date();
-  if (current.getDate() == 2 && current.getMonth() == 8) {
-    document.getElementById("birthday").innerHTML = "Today is my Birthday and I am " + (current.getFullYear() - 2003) + " yrs old now.";
 
-    //Wish Function
-    setTimeout(function () {
-      var wish = confirm("Wanna Wish me ?");
-      if (wish) {
-        window.location.href = "https://t.me/mishrabiswajit";
-      }
-      else {
-        alert("You made me cry 😭😭");
-      }
-    }, 5000);
+  if (current.getDate() >= 2 && current.getMonth() >= 8) {
+    if (current.getDate() == 2 && current.getMonth() == 8) {
+      document.getElementById("birthday").innerHTML = "Today is my Birthday and I am " + (current.getFullYear() - 2003) + " yrs old now.";
+
+      //Wish Function
+      setTimeout(function () {
+        var wish = confirm("Wanna Wish me ?");
+        if (wish) {
+          window.location.href = "https://t.me/mishrabiswajit";
+        }
+        else {
+          alert("You made me cry 😭😭");
+        }
+      }, 5000);
+    }
+    else {
+      document.getElementById("birthday").innerHTML = "I am " + (current.getFullYear() - 2003) + " yrs old.";
+    }
   }
 
   else {
