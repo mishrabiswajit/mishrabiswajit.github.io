780bca03ba841cf8cfcccc3cc08f64b8506454ac
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-08-16 14:14:11 +0530

    Lets move to Full Automation
    1. Added birthday function
    2. Added wish function
    3. Made my age live

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,5 +1,29 @@
+// Birthday Function
+let birthday = setTimeout(function () {
+  var current = new Date();
+  if (current.getDate() == 2 && current.getMonth() == 8) {
+    document.getElementById("birthday").innerHTML = "Today is my Birthday and I am " + (current.getFullYear() - 2003) + " yrs old now.";
+
+    //Wish Function
+    setTimeout(function () {
+      var wish = confirm("Wanna Wish me ?");
+      if (wish) {
+        window.location.href = "https://t.me/mishrabiswajit";
+      }
+      else {
+        alert("You made me cry 😭😭");
+      }
+    }, 5000);
+  }
+
+  else {
+    document.getElementById("birthday").innerHTML = "I am " + (current.getFullYear() - 2004) + " yrs old.";
+  }
+}, 0);
+
+// Clock Function
 function clock() {
-  let current = new Date();
+  var current = new Date();
   document.getElementById("clock").innerHTML = current.toLocaleTimeString({ timeZone: 'Asia/Kolkata' });
 }
 setInterval(clock, 1000);
