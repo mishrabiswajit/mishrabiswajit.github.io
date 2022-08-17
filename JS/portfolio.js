55ae0e969c0d1555abeac85ec6536c3461637d7b
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-08-17 14:24:08 +0530

    Fixed time not showing according to my watch

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -29,8 +29,29 @@ setTimeout(function () {
 
 // Clock Function
 function clock() {
-  var current = new Date();
-  document.getElementById("clock").innerHTML = current.toLocaleTimeString({ timeZone: 'Asia/Kolkata' });
+  var local_time = new Date(); // Getting the local time
+  var india_time = new Date(local_time.toLocaleString("en-US", { timeZone: "Asia/Kolkata" })); // Getting the indian time
+
+  // AM/PM Converter
+  var hours = india_time.getHours();
+  var minutes = india_time.getMinutes();
+  var seconds = india_time.getSeconds();
+  var AM_PM = "AM";
+  if (hours >= 12) {
+    AM_PM = "PM";
+    hours -= 12;
+  }
+
+  // Zero inserter
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  // Final String
+  document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds} ${AM_PM}`;
 
   // Random Colour Generator
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
